f89a30950c975651093656b0957b032a161b341d
kivkin <kivkin@kivkin-Inspiron-3541.(none)>
2016-03-16 20:22:21 +0300

    base

---
+++ Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+namespace /DK/Sitenews/Helper
+class Data{
+	
+}
---
+++ composer.js
@@ -0,0 +1,13 @@
+{
+    "name": "dk/module-sitenews",
+    "type": "magento2-module",
+    "require": {
+        "magento/framework": "*"
+    },
+    "autoload": {
+        "files": [ "registration.php" ],
+        "psr-4": {
+            "DK\\Sitenews\\": ""
+        }
+    }
+}
